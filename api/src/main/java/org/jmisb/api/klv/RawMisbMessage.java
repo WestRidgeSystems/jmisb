60dfb2f9535c98b3253061fc5f3abf4e9e7b4445
wlfgang <14792753+wlfgang@users.noreply.github.com>
2018-09-17 06:42:09 -0400

    KLV parser returns unsupported message types

---
+++ api/src/main/java/org/jmisb/api/klv/RawMisbMessage.java
@@ -0,0 +1,38 @@
+package org.jmisb.api.klv;
+
+/**
+ * Represents an unparsed {@link IMisbMessage}
+ */
+public class RawMisbMessage implements IMisbMessage
+{
+    private UniversalLabel universalLabel;
+    private byte[] bytes;
+
+    public RawMisbMessage(UniversalLabel universalLabel, byte[] bytes)
+    {
+        this.universalLabel = universalLabel;
+        this.bytes = bytes;
+    }
+
+    @Override
+    public UniversalLabel getUniversalLabel()
+    {
+        return universalLabel;
+    }
+
+    /**
+     * Get the raw data, including 16-byte UL and length fields
+     *
+     * @return The raw byte array
+     */
+    public byte[] getBytes()
+    {
+        return bytes;
+    }
+
+    @Override
+    public byte[] frameMessage(boolean isNested)
+    {
+        return bytes;
+    }
+}
