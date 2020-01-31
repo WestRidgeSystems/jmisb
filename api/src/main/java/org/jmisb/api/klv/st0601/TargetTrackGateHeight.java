d91308b3f79ae803d1851c7ddf67e6cc9da548bf
Brad Hards <bradh@frogmouth.net>
2020-02-01 08:12:52 +1100

    Add support for Target Track Gate Width and Height (#49)

---
+++ api/src/main/java/org/jmisb/api/klv/st0601/TargetTrackGateHeight.java
@@ -0,0 +1,33 @@
+package org.jmisb.api.klv.st0601;
+
+/**
+ * Target Track Gate Height (ST 0601 tag 44)
+ * <p>
+ * From ST:
+ * <blockquote>
+ * Tracking gate height (y value) of tracked target within field of view.
+ * Closely tied to source Motion Imagery.
+ * <p>
+ * For Target Tracking Sensors which display a box or gate around the target location,
+ * the Target Track Gate Height specifies the height in pixels for the displayed tracking
+ * gate.
+ * </blockquote>
+ */
+public class TargetTrackGateHeight extends TargetTrackGateSize
+{
+    public TargetTrackGateHeight(short px)
+    {
+        super(px);
+    }
+
+    public TargetTrackGateHeight(byte[] bytes)
+    {
+        super(bytes);
+    }
+
+    @Override
+    public String getDisplayName()
+    {
+        return "Target Track Gate Height";
+    }
+}
---
+++ api/src/main/java/org/jmisb/api/klv/st0601/TargetTrackGateWidth.java
@@ -0,0 +1,33 @@
+package org.jmisb.api.klv.st0601;
+
+
+/**
+ * Target Track Gate Width (ST 0601 tag 43)
+ * <p>
+ * From ST:
+ * <blockquote>
+ * Tracking gate width (x value) of tracked target within field of view.
+ * Closely tied to source Motion Imagery.
+ * <p>
+ * For Target Tracking Sensors which display a box or gate around the target location, the Target
+ * Track Gate Width specifies the width in pixels for the displayed tracking gate.
+ * </blockquote>
+ */
+public class TargetTrackGateWidth extends TargetTrackGateSize
+{
+    public TargetTrackGateWidth(short px)
+    {
+        super(px);
+    }
+
+    public TargetTrackGateWidth(byte[] bytes)
+    {
+        super(bytes);
+    }
+
+    @Override
+    public String getDisplayName()
+    {
+        return "Target Track Gate Width";
+    }
+}
