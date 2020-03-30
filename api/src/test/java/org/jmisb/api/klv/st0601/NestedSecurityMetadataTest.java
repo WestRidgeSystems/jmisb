be60025b9cf1c5fac07c52c55af3696619d0ed3a
Brad Hards <bradh@frogmouth.net>
2020-03-30 21:14:34 +1100

    Add test for UasDatalinkFactory creation of NestedSecurityMetadata
    Also minor test refactoring. No main/ side changes.

--- api/src/test/java/org/jmisb/api/klv/st0601/NestedSecurityMetadataTest.java
+++ api/src/test/java/org/jmisb/api/klv/st0601/NestedSecurityMetadataTest.java
@@ -12,32 +12,42 @@ import org.testng.annotations.Test;
 
 public class NestedSecurityMetadataTest
 {
+    private final byte[] localSetAsBytes = new byte[]{(byte)0x05, (byte)0x05, (byte)0x54, (byte)0x65, (byte)0x73, (byte)0x54, (byte)0x31};
+
     @Test
     public void testConstructFromLocalSet()
     {
         NestedSecurityMetadata nestedSecurityMetadata = new NestedSecurityMetadata(makeLocalSet());
-        Assert.assertNotNull(nestedSecurityMetadata);
-        Assert.assertEquals(nestedSecurityMetadata.getDisplayName(), "Security");
-        Assert.assertEquals(nestedSecurityMetadata.getDisplayableValue(), "[Security metadata]");
-        Assert.assertEquals(nestedSecurityMetadata.getLocalSet().getField(SecurityMetadataKey.Caveats).getDisplayableValue(), "TesT1");
+        checkResults(nestedSecurityMetadata);
     }
 
     @Test
     public void testConstructFromBytes() throws KlvParseException
     {
-        SecurityMetadataLocalSet localSet = makeLocalSet();
-        byte[] localSetAsByteArray = localSet.frameMessage(true);
-        NestedSecurityMetadata nestedSecurityMetadata = new NestedSecurityMetadata(localSetAsByteArray);
-        Assert.assertNotNull(nestedSecurityMetadata);
+        NestedSecurityMetadata nestedSecurityMetadata = new NestedSecurityMetadata(localSetAsBytes);
+        checkResults(nestedSecurityMetadata);
+    }
 
+    @Test
+    public void testFactory() throws KlvParseException
+    {
+        IUasDatalinkValue value = UasDatalinkFactory.createValue(UasDatalinkTag.SecurityLocalMetadataSet, localSetAsBytes);
+        Assert.assertTrue(value instanceof NestedSecurityMetadata);
+        NestedSecurityMetadata nestedSecurityMetadata = (NestedSecurityMetadata)value;
+        checkResults(nestedSecurityMetadata);
+    }
+
+    private void checkResults(NestedSecurityMetadata nestedSecurityMetadata) {
+        Assert.assertNotNull(nestedSecurityMetadata);
         Assert.assertEquals(nestedSecurityMetadata.getDisplayName(), "Security");
         Assert.assertEquals(nestedSecurityMetadata.getDisplayableValue(), "[Security metadata]");
         Assert.assertEquals(nestedSecurityMetadata.getLocalSet().getField(SecurityMetadataKey.Caveats).getDisplayableValue(), "TesT1");
         byte[] bytes = nestedSecurityMetadata.getBytes();
-        Assert.assertEquals(bytes, localSetAsByteArray);
+        Assert.assertEquals(bytes, localSetAsBytes);
     }
 
-    private SecurityMetadataLocalSet makeLocalSet() {
+    private SecurityMetadataLocalSet makeLocalSet()
+    {
         SortedMap<SecurityMetadataKey, ISecurityMetadataValue> securityKeyValuePairs = new TreeMap<>();
         securityKeyValuePairs.put(SecurityMetadataKey.Caveats, new SecurityMetadataString("TesT1"));
         SecurityMetadataLocalSet securityMetadataLocalSet = new SecurityMetadataLocalSet(securityKeyValuePairs);
