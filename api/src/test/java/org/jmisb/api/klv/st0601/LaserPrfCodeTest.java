046ef8940098b505ac7368dcfc148093bb5d4a2c
Brad Hards <bradh@frogmouth.net>
2020-04-08 21:43:18 +1000

    Implement additional PRF Laser Code check (#90)

--- api/src/test/java/org/jmisb/api/klv/st0601/LaserPrfCodeTest.java
+++ api/src/test/java/org/jmisb/api/klv/st0601/LaserPrfCodeTest.java
@@ -74,19 +74,31 @@ public class LaserPrfCodeTest
         Assert.assertEquals(code.getDisplayableValue(), "1743");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Laser PRF Code must be in the range \\[111, 8888\\]")
     public void testTooSmall()
     {
         new LaserPrfCode(110);
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Laser PRF Code must be in the range \\[111, 8888\\]")
     public void testTooBig()
     {
         new LaserPrfCode(8889);
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Laser PRF Code must only contain 1 through 8, not 0 or 9")
+    public void testHasZero()
+    {
+        new LaserPrfCode(1130);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Laser PRF Code must only contain 1 through 8, not 0 or 9")
+    public void testHasNine()
+    {
+        new LaserPrfCode(1932);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Laser PRF Code encoding is a 2-byte unsigned int")
     public void badArrayLength()
     {
         new LaserPrfCode(new byte[]{0x00});
