4eac40559dc03bc4b05aa4b8bb00f295613690d3
Brad Hards <bradh@frogmouth.net>
2020-04-30 19:30:55 +1000

    Bump ST_VERSION_NUMBER to match ST0601.16

--- api/src/main/java/org/jmisb/api/klv/st0601/UasDatalinkConstants.java
+++ api/src/main/java/org/jmisb/api/klv/st0601/UasDatalinkConstants.java
@@ -1,12 +1,12 @@
 package org.jmisb.api.klv.st0601;
 
 /**
- * Constants used by ST 0601
+ * Constants used by ST 0601.
  */
 public class UasDatalinkConstants
 {
     private UasDatalinkConstants() {}
 
-    /** The currently-supported revision is 0601.15 */
-    public static byte ST_VERSION_NUMBER = 15;
+    /** The currently-supported revision is 0601.16 */
+    public static byte ST_VERSION_NUMBER = 16;
 }
