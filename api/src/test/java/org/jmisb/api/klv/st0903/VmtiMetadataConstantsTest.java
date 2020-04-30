2c4fbace54ea6b15ebeada327ebd1d8fead53708
Brad Hards <bradh@frogmouth.net>
2020-05-01 06:14:56 +1000

    Add trivial test for VMTI constants (#115)

---
+++ api/src/test/java/org/jmisb/api/klv/st0903/VmtiMetadataConstantsTest.java
@@ -0,0 +1,16 @@
+package org.jmisb.api.klv.st0903;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for VmtiMetadataConstants.
+ */
+public class VmtiMetadataConstantsTest
+{    
+    @Test
+    public void checkVersion()
+    {
+        assertEquals(VmtiMetadataConstants.ST_VERSION_NUMBER, 5);
+    }
+}
