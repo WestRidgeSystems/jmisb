c1ecb5bce4879a990483392e16a2c8956061755e
Brad Hards <bradh@frogmouth.net>
2020-05-09 21:20:36 +1000

    Implement metadata keys for ST0806

---
+++ api/src/main/java/org/jmisb/api/klv/st0806/RvtMetadataKey.java
@@ -0,0 +1,63 @@
+package org.jmisb.api.klv.st0806;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ST 0806 tags - description and numbers.
+ */
+public enum RvtMetadataKey
+{
+    /**
+     * Unknown key. This should not be created.
+     */
+    Undefined(0),
+    CRC32(1),
+    UserDefinedTimeStampMicroseconds(2),
+    PlatformTrueAirspeed(3),
+    PlatformIndicatedAirspeed(4),
+    TelemetryAccuracyIndicator(5),
+    FragCircleRadius(6),
+    FrameCode(7),
+    UASLSVersionNumber(8),
+    VideoDataRate(9),
+    DigitalVideoFileFormat(10),
+    UserDefinedLS(11),
+    PointOfInterestLS(12),
+    AreaOfInterestLS(13),
+    MGRSZone(14),
+    MGRSLatitudeBandAndGridSquare(15),
+    MGRSEasting(16),
+    MGRSNorthing(17),
+    MGRSZoneSecondValue(18),
+    MGRSLatitudeBandAndGridSquareSecondValue(19),
+    MGRSEastingSecondValue(20),
+    MGRSNorthingSecondValue(21);
+
+    private int tag;
+
+    private static final Map<Integer, RvtMetadataKey> tagTable = new HashMap<>();
+
+    static
+    {
+        for (RvtMetadataKey key : values())
+        {
+            tagTable.put(key.tag, key);
+        }
+    }
+
+    RvtMetadataKey(int tag)
+    {
+        this.tag = tag;
+    }
+
+    public int getTag()
+    {
+        return tag;
+    }
+
+    public static RvtMetadataKey getKey(int tag)
+    {
+        return tagTable.containsKey(tag) ? tagTable.get(tag) : Undefined;
+    }
+}
---
+++ api/src/test/java/org/jmisb/api/klv/st0806/RvtMetadataKeyTest.java
@@ -0,0 +1,57 @@
+package org.jmisb.api.klv.st0806;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests for RvtMetadataKey.
+ */
+public class RvtMetadataKeyTest {
+
+    @Test
+    public void Enum0Test()
+    {
+        RvtMetadataKey key = RvtMetadataKey.getKey(0);
+        assertEquals(key, RvtMetadataKey.Undefined);
+        assertEquals(key.getTag(), 0);
+    }
+
+    @Test
+    public void EnumUnknownTest()
+    {
+        RvtMetadataKey key = RvtMetadataKey.getKey(999);
+        assertEquals(key, RvtMetadataKey.Undefined);
+        assertEquals(key.getTag(), 0);
+    }
+
+    @Test
+    public void Enum1Test()
+    {
+        RvtMetadataKey key = RvtMetadataKey.getKey(1);
+        assertEquals(key, RvtMetadataKey.CRC32);
+        assertEquals(key.getTag(), 1);
+    }
+
+    @Test
+    public void Enum2Test()
+    {
+        RvtMetadataKey key = RvtMetadataKey.getKey(2);
+        assertEquals(key, RvtMetadataKey.UserDefinedTimeStampMicroseconds);
+        assertEquals(key.getTag(), 2);
+    }
+
+    @Test
+    public void Enum20Test()
+    {
+        RvtMetadataKey key = RvtMetadataKey.getKey(20);
+        assertEquals(key, RvtMetadataKey.MGRSEastingSecondValue);
+        assertEquals(key.getTag(), 20);
+    }
+
+    public void Enum21Test()
+    {
+        RvtMetadataKey key = RvtMetadataKey.getKey(21);
+        assertEquals(key, RvtMetadataKey.MGRSNorthingSecondValue);
+        assertEquals(key.getTag(), 21);
+    }
+}
