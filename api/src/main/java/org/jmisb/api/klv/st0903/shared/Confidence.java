516654372979e8d626683a3f0dd5233655c6f73a
Brad Hards <bradh@frogmouth.net>
2020-06-02 06:29:54 +1000

    Minor header cleanups (#142)

--- api/src/main/java/org/jmisb/api/klv/st0903/shared/Confidence.java
+++ api/src/main/java/org/jmisb/api/klv/st0903/shared/Confidence.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jmisb.api.klv.st0903.shared;
 
 import org.jmisb.api.klv.st0903.IVmtiMetadataValue;
