75950cbef92dac21d0e016f7f50ddb9e2e841a37
Brad Hards <bradh@frogmouth.net>
2020-08-17 13:53:54 +1000

    examples: add System.out example code

---
+++ examples/README.md
@@ -0,0 +1,17 @@
+# Examples for jMISB
+
+This is demonstration / example code for <https://github.com/WestRidgeSystems/jmisb>
+
+Examples are intended to be illustrative of API use. They prefer simplicity over completeness. They don't always handle error conditions, and should not be considered production-level code.
+
+Examples are not considered part of the API, and may be changed or removed in minor versions.
+
+In addition, some examples may be work-in-progress (WIP). They should build, but may not run.
+
+We welcome corrections and updates.
+
+## systemout
+
+It does a console dump of the KLV metadata in a file to standard output (`System.out` in Java, hence the name). This example is considered complete. See <systemout/README.md> for more information.
+
+
---
+++ examples/systemout/src/main/java/org/jmisb/examples/systemout/MetadataPlayer.java
@@ -0,0 +1,95 @@
+package org.jmisb.examples.systemout;
+
+import java.io.IOException;
+import org.jmisb.api.common.InvalidDataHandler;
+import org.jmisb.api.klv.IKlvKey;
+import org.jmisb.api.klv.IKlvValue;
+import org.jmisb.api.klv.IMisbMessage;
+import org.jmisb.api.klv.INestedKlvValue;
+import org.jmisb.api.video.IFileEventListener;
+import org.jmisb.api.video.IMetadataListener;
+import org.jmisb.api.video.IVideoFileInput;
+import org.jmisb.api.video.MetadataFrame;
+import org.jmisb.api.video.VideoFileInput;
+import org.jmisb.api.video.VideoFileInputOptions;
+import org.jmisb.core.klv.ArrayUtils;
+
+public class MetadataPlayer implements IMetadataListener, IFileEventListener {
+
+    private IVideoFileInput fileInput;
+
+    public MetadataPlayer() {
+        setUpErrorHandlers();
+    }
+
+    private void setUpErrorHandlers() {
+        InvalidDataHandler.getInstance()
+                .setInvalidFieldEncodingStrategy(new PrintOnInvalidDataStrategy());
+        InvalidDataHandler.getInstance()
+                .setInvalidChecksumStrategy(new PrintOnInvalidDataStrategy());
+    }
+
+    public void play(String filename) throws IOException {
+        // These options configure for metadata decode only
+        VideoFileInputOptions videoFileInputOptions =
+                new VideoFileInputOptions(false, true, false, false);
+        fileInput = new VideoFileInput(videoFileInputOptions);
+        fileInput.addMetadataListener(this);
+        fileInput.addFileEventListener(this);
+        fileInput.open(filename);
+    }
+
+    @Override
+    // This is from IMetadataListener
+    public void onMetadataReceived(MetadataFrame metadataFrame) {
+        outputTopLevelMessageHeader(metadataFrame.getMisbMessage());
+        outputNestedKlvValue(metadataFrame.getMisbMessage(), 1);
+    }
+
+    private void outputTopLevelMessageHeader(IMisbMessage misbMessage) {
+        String displayHeader = misbMessage.displayHeader();
+        if (displayHeader.equalsIgnoreCase("Unknown")) {
+            System.out.println(
+                    displayHeader
+                            + " ["
+                            + ArrayUtils.toHexString(misbMessage.getUniversalLabel().getBytes())
+                                    .trim()
+                            + "]");
+            outputUnknownMessageContent(misbMessage.frameMessage(true));
+        } else {
+            System.out.println(displayHeader);
+        }
+    }
+
+    private void outputUnknownMessageContent(byte[] frameMessage) {
+        System.out.println(ArrayUtils.toHexString(frameMessage, 16, true));
+    }
+
+    @Override
+    // This is from IFileEventListener
+    public void onEndOfFile() {
+        try {
+            fileInput.close();
+        } catch (IOException ex) {
+            System.out.println("Failed to close file: " + ex.getMessage());
+        }
+    }
+
+    private void outputNestedKlvValue(INestedKlvValue nestedKlvValue, int indentationLevel) {
+        for (IKlvKey identifier : nestedKlvValue.getIdentifiers()) {
+            IKlvValue value = nestedKlvValue.getField(identifier);
+            outputValueWithIndentation(value, indentationLevel);
+            // if this has nested content, output that at the next indentation level
+            if (value instanceof INestedKlvValue) {
+                outputNestedKlvValue((INestedKlvValue) value, indentationLevel + 1);
+            }
+        }
+    }
+
+    private void outputValueWithIndentation(IKlvValue value, int indentationLevel) {
+        for (int i = 0; i < indentationLevel; ++i) {
+            System.out.print("\t");
+        }
+        System.out.println(value.getDisplayName() + ": " + value.getDisplayableValue());
+    }
+}
