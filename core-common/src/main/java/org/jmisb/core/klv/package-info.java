72ecea86f2463585fdd12809a6eca79014ffa03b
Brad Hards <bradh@frogmouth.net>
2021-06-05 03:13:10 +1000

    core: split out non-ffmpeg core to core-common (#292)

---
+++ core-common/src/main/java/org/jmisb/core/klv/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Utility classes for common metadata operations.
+ *
+ * <p>This package provides shared implementation of methods that are commonly needed in KLV and
+ * other metadata processing, such as operations on byte arrays.
+ *
+ * <p>In jMISB 1.10 and earlier, this functionality was provided in the jmisb-core artifact. It was
+ * split out to enable use without the ffmpeg dependency.
+ */
+package org.jmisb.core.klv;
---
+++ core-common/src/main/java/org/jmisb/core/video/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Utility classes for common video operations.
+ *
+ * <p>This package provides shared implementation of methods that are commonly needed in video
+ * processing.
+ *
+ * <p>In jMISB 1.10 and earlier, this functionality was provided in the jmisb-core artifact. It was
+ * split out to enable use without the ffmpeg dependency.
+ */
+package org.jmisb.core.video;
