01dfb88051de1d24218e26f8d209f4681741eac4
Brad Hards <bradh@frogmouth.net>
2022-05-12 21:29:58 +1000

    st0102: add validator

---
+++ st0102/src/main/java/org/jmisb/st0102/validity/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * ST 0102 Validation.
+ *
+ * <p>ST 0102 has required elements. This package allows checking that a set is valid against
+ * reasonably checkable rules.
+ *
+ * <p>It does not validate that the classification and other security metadata values are
+ * appropriate or reasonable, only that they are present.
+ */
+package org.jmisb.st0102.validity;
